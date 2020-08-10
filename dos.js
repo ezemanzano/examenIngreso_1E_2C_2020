b88cd43ccf66b4bc93a99b3cadcb6e2dd3bdbb0f
ezemanzano <ezequielm789@gmail.com>
2020-08-10 20:12:04 -0300

    Fin parcial

--- dos.js
+++ dos.js
@@ -1,11 +1,68 @@
 /*
 2)De una compra debes ingresar una cantidad indeterminada de productos,
  validando los siguientes datos:
-marca, precio, peso en kilogramos, tipo(sólido o líquido)
+marca, 
+precio, 
+peso en kilogramos,
+ tipo(sólido o líquido)
 a)informar el peso total de la compra.
 b)la marca del más caro de los líquidos
 c)la marca del más barato de los sólidos
-*/function mostrar()
-{
-  alert("dos");
+*/
+function mostrar() {
+  let precio;
+  let marca;
+  let peso;
+  let tipo;
+  let respuesta = "s";
+  let acumPeso = 0;
+  let flag1 = 0;
+  let liquidoCaro;
+  let marcaCara;
+  let bandera = 0;
+  let solidoBarato = 0;
+  let marcaBarato;
+  do {
+    do {
+      marca = prompt("Ingrese marca \n solo caracteres");
+    } while (!(isNaN(marca)));
+    do {
+      precio = (parseFloat(prompt("Ingrese precio: ")));
+    } while (!(precio > 0) && isNaN(precio));
+    do {
+      peso = parseFloat(prompt("Ingrese peso: "));
+    } while (!(peso > 0));
+    do {
+      tipo = prompt("Ingrese tipo : \n (solido o liquido)");
+    } while (!(tipo == "liquido" || tipo == "solido"));
+
+
+    respuesta = prompt("desea continuar? s/n");
+
+    acumPeso += peso;
+    if (tipo == "liquido") {
+      if (flag1 == 0 || liquidoCaro < precio) {
+        liquidoCaro = precio;
+        marcaCara = marca;
+        flag1 = 1;
+      }
+    } else {
+      if (bandera == 0 || solidoBarato > precio) {
+        solidoBarato = precio;
+        marcaBarato = marca;
+        bandera = 1;
+      }
+    }
+  } while (respuesta == "s");
+  document.write("El Peso total de la compra es: " + acumPeso + "<br>");
+  if (flag1 == 0) {
+    document.write("No Ingresó ningún liquido" + "<br>");
+  } else {
+    document.write("La marca más cara de todos los líquidos es " + marcaCara + "  Con $ " + liquidoCaro + "<br>");
+  }
+  if (bandera == 0) {
+    document.write("No Ingresó ningún sólido" + "<br>");
+  } else {
+    document.write("La marca más  barata de todos los sólidos es " + marcaBarato + "  Con $ " + solidoBarato + "<br>");
+  }
 }
--- tres.js
+++ tres.js
@@ -1,7 +1,8 @@
 /*
 3)Nos ingresan una cantidad indeterminada de estadías de  vacaciones, 
 validando los datos ingresados. 
-sexo del titular ,lugar ( “bariloche”, “cataratas” o “salta”),
+sexo del titular ,
+lugar ( “bariloche”, “cataratas” o “salta”),
 temporada(“otoño”,”invierno, “verano,”primavera”),
 cantidad de personas que viajan.
 informar:
@@ -9,7 +10,79 @@ a)el lugar más elegido
 b)el sexo de titular que lleva más pasajeros.
 c)el promedio de personas por viaje,  que viajan en invierno
 */
-function mostrar()
-{
-	alert("tres");
+function mostrar() {
+	let sexo;
+	let lugar;
+	let temporada;
+	let cantidad;
+	let respuesta = "s";
+	let contBar = 0;
+	let contCat = 0;
+	let contSal = 0;
+	let flag1 = 0;
+	let sexoPasajeros;
+	let masPasajeros = 0;
+	let acumInv = 0;
+	let contInv = 0;
+
+	do {
+		do {
+			sexo = prompt("Ingrese sexo del titular: \n (f/m)");
+		} while (!(sexo == "f" || sexo == "m"));
+		do {
+			lugar = prompt("Ingrese lugar de destino: \n (bariloche/salta/cataratas)");
+		} while (!(lugar == "bariloche" || lugar == "cataratas" || lugar == "salta"));
+		do {
+			temporada = prompt("En que temporada desea ir ? \n (Verano/primavera/otoño/Invierno) ");
+
+		} while (!(temporada == "verano" || temporada == "otoño" || temporada == "primavera" || temporada == "invierno"));
+		do {
+			cantidad = parseInt(prompt("Ingrese cantidad: "))
+		} while (!(cantidad > 0));
+		respuesta = prompt("Desea continuar? s/n");
+
+		// a)el lugar más elegido
+		if (lugar == "bariloche") {
+			contBar++;
+		} else if (lugar == "cataratas") {
+			contCat++;
+		} else {
+			contSal++;
+		}
+
+		if (flag1 == 0 || masPasajeros < cantidad) {
+			masPasajeros = cantidad;
+			sexoPasajeros = sexo;
+			flag1 = 1;
+		}
+
+		if (temporada == "invierno") {
+			acumInv += cantidad;
+			contInv++;
+		}
+
+	} while (respuesta == "s");
+
+	//a)
+	if (sexoPasajeros = "f") {
+		sexoPasajeros = "Femenino";
+	} else {
+		sexoPasajeros = "Masculino";
+	}
+	
+	if (contBar > contCat && contBar > contSal) {
+		alert("El lugar mas elegido es Bariloche");
+	} else if (contCat >= contBar && contCat > contSal) {
+		prompt("El lugar mas elgido es Cataratas");
+	} else {
+		alert("El lugar mas elegido es Salta");
+	}
+	
+	alert("El sexo que lleva más pasajeros es: " + sexoPasajeros + " con " + masPasajeros);
+	if (acumInv != 0) {
+		acumInv = acumInv / contInv;
+		alert("El promedio de personas que viajan en invierno es: "+ acumInv);
+	} else {
+		alert("No viajan personas en Invierno");
+	}
 }
